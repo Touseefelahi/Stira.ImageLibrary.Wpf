9ea3e5b7fd155634cfc44ffbde9f288ee334b2ab
Touseef Elahi <touseefelahi@ymail.com>
2020-09-08 11:15:52 +0300

    Bitmap source convert added

---
+++ Stira.ImageLibrary.Wpf/Converters/MatToBitmapSourceConverter.cs
@@ -0,0 +1,42 @@
+﻿using Emgu.CV;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Stira.ImageLibrary.Wpf
+{
+    public static class BitmapSourceConvert
+    {
+        public static BitmapSource ToBitmapSource(Mat image)
+        {
+            using System.Drawing.Bitmap source = image.ToBitmap();
+            IntPtr ptr = source.GetHbitmap();
+
+            BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ptr);
+            return bs;
+        }
+
+        public static BitmapSource ToBitmapSource(System.Drawing.Bitmap source)
+        {
+            IntPtr ptr = source.GetHbitmap();
+            BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ptr);
+            return bs;
+        }
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+    }
+}
